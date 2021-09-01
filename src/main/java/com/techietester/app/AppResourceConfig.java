d67e8db003bf33429fe5056160fddf151eef082d
dtacademyb5 <86332147+dtacademyb5@users.noreply.github.com>
2021-08-31 23:00:59 -0400

    Update AppResourceConfig.java

--- src/main/java/com/techietester/app/AppResourceConfig.java
+++ src/main/java/com/techietester/app/AppResourceConfig.java
@@ -27,7 +27,7 @@ public class AppResourceConfig extends ResourceConfig {
         beanConfig.setVersion("1.0.0");
         beanConfig.setSchemes(new String[] { "http", "https" });
         beanConfig.setBasePath("/app");
-        beanConfig.setTitle("Video Game Database - Test Application");
+        beanConfig.setTitle("Video Games API");
         beanConfig.getSwagger().addConsumes(MediaType.APPLICATION_JSON);
         beanConfig.getSwagger().addProduces(MediaType.APPLICATION_JSON);
         beanConfig.setResourcePackage("com.techietester.resource");
